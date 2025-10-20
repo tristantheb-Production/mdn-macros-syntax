ef19594d760193b7d9b186409a2edbb6dca34044
Tristan D. <tristantheb@users.noreply.github.com>
2025-10-20 04:22:19 +0200

    chore(deps): linter update with new configs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,3 +1,4 @@
+const stylistic = require('@stylistic/eslint-plugin')
 const tsParser = require('@typescript-eslint/parser');
 const tsPlugin = require('@typescript-eslint/eslint-plugin');
 
@@ -21,50 +22,45 @@ module.exports = [
         require: 'readonly'
       }
     },
-    plugins: { '@typescript-eslint': tsPlugin },
-    rules: {
-      'quotes': ['error', 'single', { 'avoidEscape': true }],
-      'indent': ['error', 2, { 'SwitchCase': 1 }],
-      'comma-dangle': ['error', 'never'],
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
-      'no-unused-vars': 'off',
-      'no-undef': 'off'
-    }
-  },
-  {
-    files: ['src/test/**/*.ts', 'src/test/**/*.tsx'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: __dirname,
-        ecmaVersion: 2021,
-        sourceType: 'module'
-      },
-      globals: {
-        describe: 'readonly',
-        it: 'readonly',
-        before: 'readonly',
-        after: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
-        process: 'readonly',
-        console: 'readonly',
-        __dirname: 'readonly',
-        module: 'readonly',
-        require: 'readonly'
-      }
+    plugins: {
+      '@stylistic': stylistic,
+      '@typescript-eslint': tsPlugin
     },
-    plugins: { '@typescript-eslint': tsPlugin },
     rules: {
-      'quotes': ['error', 'single', { 'avoidEscape': true }],
-      'indent': ['error', 2, { 'SwitchCase': 1 }],
       'comma-dangle': ['error', 'never'],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'never'],
       '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
-      'no-unused-vars': 'off',
-      'no-undef': 'off'
+      '@typescript-eslint/no-unused-vars': [
+        'error', { 'argsIgnorePattern': '^_' }
+      ],
+      '@stylistic/no-trailing-spaces': 'error',
+      'no-unused-vars': 'error',
+      'no-undef': 'error'
     }
   }
 ];
+
+// Add Mocha test globals for test files
+module.exports.push({
+  files: ['src/test/**/*.ts'],
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      project: './tsconfig.json',
+      tsconfigRootDir: __dirname,
+      ecmaVersion: 2021,
+      sourceType: 'module'
+    },
+    globals: {
+      describe: 'readonly',
+      it: 'readonly',
+      before: 'readonly',
+      after: 'readonly',
+      beforeEach: 'readonly',
+      afterEach: 'readonly'
+    }
+  }
+});
